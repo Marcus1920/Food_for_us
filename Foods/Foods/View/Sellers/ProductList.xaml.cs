a125a9f8bc3450fd133886b709ab86c591bc2967
Babalo Cetywa <babalocetywa@gmail.com>
2017-08-17 17:15:54 +0200

    Add ListView for Products and Transactions Lists in Sellers' Interface.

---
+++ Foods/Foods/View/Sellers/ProductList.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Foods.View.Sellers
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProductList : ContentPage
+    {
+        List<string> GetDummyList()
+        {
+            return new List<string>
+            {
+                "First Product",
+                "Second Product",
+                "Third Product",
+                "Fourth Product",
+                "Fifth Product"
+            };
+        }
+
+        public ProductList()
+        {
+            InitializeComponent();
+            
+            ProductsListView.ItemsSource = GetDummyList();
+        }
+
+        private void ProductsListView_OnRefreshing(object sender, EventArgs e)
+        {
+            ProductsListView.ItemsSource = GetDummyList();
+            ProductsListView.EndRefresh();
+        }
+    }
+}
---
+++ Foods/Foods/View/Sellers/TransactionList.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Foods.View.Sellers
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TransactionList : ContentPage
+    {
+        List<string> GetDummyList()
+        {
+            return new List<string>
+            {
+                "First Transaction",
+                "Second Transaction",
+                "Third Transaction",
+                "Fourth Transaction",
+                "Fifth Transaction"
+            };
+        }
+
+        public TransactionList()
+        {
+            InitializeComponent();
+
+            TransactionsListView.ItemsSource = GetDummyList();
+        }
+
+        private void TransactionsListView_OnRefreshing(object sender, EventArgs e)
+        {
+            TransactionsListView.ItemsSource = GetDummyList();
+            TransactionsListView.EndRefresh();
+        }
+    }
+}
