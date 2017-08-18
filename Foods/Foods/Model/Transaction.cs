9126e98a14a3c6016eb11de354daf4e70ac974a1
Babalo Cetywa <babalocetywa@gmail.com>
2017-08-18 14:23:24 +0200

    Pupolate Transactions List with Dummy Data.

---
+++ Foods/Foods/Model/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Foods.Model
+{
+    public class Transaction
+    {
+        public string CustomerImageUrl { get; set; }
+        public string CustomerName { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public bool IsComplete { get; set; }
+        public bool IsInProgress => !this.IsComplete;
+    }
+}
--- Foods/Foods/View/Sellers/TransactionList.xaml.cs
+++ Foods/Foods/View/Sellers/TransactionList.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System;
+﻿using Foods.Model;
+using System;
 using System.Collections.Generic;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -9,18 +9,6 @@ namespace Foods.View.Sellers
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class TransactionList : ContentPage
     {
-        List<string> GetDummyList()
-        {
-            return new List<string>
-            {
-                "First Transaction",
-                "Second Transaction",
-                "Third Transaction",
-                "Fourth Transaction",
-                "Fifth Transaction"
-            };
-        }
-
         public TransactionList()
         {
             InitializeComponent();
@@ -32,6 +20,77 @@ namespace Foods.View.Sellers
         {
             TransactionsListView.ItemsSource = GetDummyList();
             TransactionsListView.EndRefresh();
+        }
+
+        List<Transaction> GetDummyList()
+        {
+            return new List<Transaction>
+            {
+                new Transaction
+                {
+                    CustomerImageUrl = "b1.png",
+                    CustomerName = "Babalo Cetywa",
+                    Description = "10kg Apples requested..",
+                    Date = DateTime.Now.AddDays(-2).AddHours(+3),
+                    IsComplete = true
+                },
+                new Transaction
+                {
+                    CustomerImageUrl = "b1.png",
+                    CustomerName = "Fellow Mtshali",
+                    Description = "5kg Oranges requested..",
+                    Date = DateTime.Now.AddDays(-1).AddHours(+5),
+                    IsComplete = false
+                },
+                new Transaction
+                {
+                    CustomerImageUrl = "b1.png",
+                    CustomerName = "Brian Thomas",
+                    Description = "25kg Grapes requested..",
+                    Date = DateTime.Now.AddDays(-4).AddHours(+3),
+                    IsComplete = true
+                },
+                new Transaction
+                {
+                    CustomerImageUrl = "b1.png",
+                    CustomerName = "Marcus Mbayo",
+                    Description = "10kg Apples requested..",
+                    Date = DateTime.Now.AddDays(-2).AddHours(+3),
+                    IsComplete = true
+                },
+                new Transaction
+                {
+                    CustomerImageUrl = "b1.png",
+                    CustomerName = "Babalo Cetywa",
+                    Description = "10kg Apples requested..",
+                    Date = DateTime.Now.AddDays(-2).AddHours(+3),
+                    IsComplete = true
+                },
+                new Transaction
+                {
+                    CustomerImageUrl = "b1.png",
+                    CustomerName = "Olwethu Ngobese",
+                    Description = "10kg Apples requested..",
+                    Date = DateTime.Now.AddDays(-2).AddHours(+3),
+                    IsComplete = false
+                },
+                new Transaction
+                {
+                    CustomerImageUrl = "b1.png",
+                    CustomerName = "Babalo Cetywa",
+                    Description = "10kg Apples requested..",
+                    Date = DateTime.Now.AddDays(-2).AddHours(+3),
+                    IsComplete = true
+                },
+                new Transaction
+                {
+                    CustomerImageUrl = "b1.png",
+                    CustomerName = "Babalo Cetywa",
+                    Description = "10kg Apples requested..",
+                    Date = DateTime.Now.AddDays(-2).AddHours(+3),
+                    IsComplete = false
+                },
+            };
         }
     }
 }
