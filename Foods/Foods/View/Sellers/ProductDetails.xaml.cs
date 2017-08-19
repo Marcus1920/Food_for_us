4c10bed472e54b848419238fe202a6dd77dcb9e4
Babalo Cetywa <babalocetywa@gmail.com>
2017-08-19 23:57:04 +0200

    Add  Action Sheet to Product Details View.

--- Foods/Foods/View/Sellers/ProductDetails.xaml.cs
+++ Foods/Foods/View/Sellers/ProductDetails.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Foods.Model;
 using System;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -13,6 +14,24 @@ namespace Foods.View.Sellers
             BindingContext = product ?? throw new ArgumentNullException();
 
             InitializeComponent();
+        }
+
+        private async Task Button_OnClicked(object sender, EventArgs e)
+        {
+            var respone = await DisplayActionSheet("ACTIONS", "Cancel", null, "Edit", "Save", "Delete");
+
+            if (!respone.Equals("Cancel"))
+            {
+                if (respone.Equals("Delete"))
+                {
+                    var delete = await DisplayAlert(respone + "?", "Are you want to remove this post?", "Yes", "No");
+                    if (delete)
+                        await DisplayAlert(null, respone + " functionality coming soon!", "Ok");
+                }
+
+                else
+                    await DisplayAlert(null, respone + " functionality coming soon!", "Ok");
+            }
         }
     }
 }
